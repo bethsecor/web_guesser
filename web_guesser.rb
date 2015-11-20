487d1c8fe43e45557a97c4afe1f650e9c97f6bec
Beth Secor <elizabeth.a.secor@gmail.com>
2015-11-20 12:00:03 -0700

    I2: uses an ERB template and HTML

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_number = rand(101)
+random_number = rand(100)
 
 get '/' do
-  "The SECRET NUMBER is #{random_number}"
+  erb :index, :locals => {:random_number => random_number}
 end
