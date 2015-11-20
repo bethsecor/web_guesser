da268d6b6ebb21d2c659e1cc5c2e751b6e364baa
Beth Secor <elizabeth.a.secor@gmail.com>
2015-11-20 14:23:57 -0700

    I4: colorizes the output

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_number = rand(100)
+SECRET_NUMBER = rand(100)
+
+def check_result(guess)
+  guess = guess.to_i
+  difference = guess - SECRET_NUMBER
+  if difference > 5
+    "Way too high!"
+  elsif difference > 0
+    "Too high!"
+  elsif difference < -5
+    "Way too low!"
+  elsif difference < 0
+    "Too low!"
+  else
+    "The SECRET NUMBER is #{SECRET_NUMBER}."
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:random_number => random_number}
+  guess = params["guess"]
+  result = check_result(guess)
+  erb :index, :locals => {:secret_number => SECRET_NUMBER,
+                          :result => result}
 end
